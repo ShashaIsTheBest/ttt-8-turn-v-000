15838b662ada66d85c11c8a21596ab2eccc066c3
Shasha Yan <shashayan@gmail.com>
2021-05-12 01:07:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,13 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(user_input)
-  user_input.to_i-1
+  user_input.to_i-1
 end
 
 def valid_move?(board,index)
@@ -27,9 +27,9 @@ def turn(board)
 end
 
 def position_taken?(board, index)
-  board[index] != " "
+  board[index] != " "
 end
 
 def move(board, index, current_player = "X")
-  board[index] = current_player
+  board[index] = current_player
 end
