aa7fe990334866dec8ac370897be332b65018192
Shasha Yan <shashayan@gmail.com>
2021-05-12 00:51:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,11 +11,7 @@ def input_to_index(user_input)
 end
 
 def valid_move?(board,index)
-  if index.between?(0,8)
-    if !position_taken?(board,index)
-      true
-    end
-  end
+  index.between?(0,8) && !position_taken?(board,index)
 end
 
 def turn(board)
